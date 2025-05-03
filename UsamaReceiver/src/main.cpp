2800b873af3994b9253061e17000e861391bc309
Tanvir Ahmed <t.a.prova@gmail.com>
2025-05-03 16:47:51 +0600

    updating the previous verson so that more data can be sent using the same method

--- UsamaReceiver/src/main.cpp
+++ UsamaReceiver/src/main.cpp
@@ -9,20 +9,24 @@ void setup() {
 
 void loop() {
   if (IrReceiver.decode()) {
-    Serial.print("Raw received value: ");
-    Serial.println(IrReceiver.decodedIRData.decodedRawData, HEX);
-    Serial.print("Protocol used: ");
-    Serial.println(IrReceiver.decodedIRData.protocol);
+    //Serial.print("Raw received value: ");
+    //Serial.println(IrReceiver.decodedIRData.decodedRawData, HEX);
+    //Serial.print("Protocol used: ");
+    //Serial.println(IrReceiver.decodedIRData.protocol);
     IrReceiver.resume(); // Prepare for the next value
     if (IrReceiver.decodedIRData.protocol == SAMSUNG) {
       Serial.println("Samsung protocol detected.");
       Serial.println(IrReceiver.decodedIRData.address, HEX);
-      Serial.println(IrReceiver.decodedIRData.command, HEX);
-      Serial.print("mq135 (0 to 255): ");
-      Serial.print(IrReceiver.decodedIRData.address, DEC);
-      Serial.print(" mq6 (0 to 255): ");
-      Serial.print(IrReceiver.decodedIRData.command, DEC);
-      Serial.println();
+      //now we can modify the data like this:
+      //the data will have a serial and data
+      //the data will be stored in address and value will be stored in command
+      int sensor_serial = IrReceiver.decodedIRData.address;
+      int sensor_data = IrReceiver.decodedIRData.command;
+
+      Serial.print("Sensor serial: ");
+      Serial.println(sensor_serial, DEC);
+      Serial.print("Sensor data: ");
+      Serial.println(sensor_data, DEC);
     }
   }
 }
--- UsamaSender/src/main.cpp
+++ UsamaSender/src/main.cpp
@@ -2,6 +2,9 @@
 
 int IR_SEND_PIN = 3;
 
+const int numberOfSensors = 4;
+int sensorData[numberOfSensors];
+
 void setup() {
   IrSender.begin(IR_SEND_PIN);
   Serial.begin(115200);
@@ -10,18 +13,26 @@ void setup() {
 void loop() {
   int mq135data  = analogRead(A0); // Read the value from the MQ135 sensor
   int mq6data    = analogRead(A1); // Read the value from the MQ6 sensor
-  uint8_t address = 0x07;  // example address
-  uint8_t command = 0x45;  // example command
+  int mq2data    = analogRead(A2); // Read the value from the MQ2 sensor
+  sensorData[0] = mq135data;
+  sensorData[1] = mq6data;
+  sensorData[2] = mq2data;
 
-  address = mq135data >> 2;
-  command = mq6data >> 2; // Shift the sensor data to fit into the command
-  
-  Serial.print("Sending Samsung IR: ");
-  Serial.print("Address = 0x");
-  Serial.print(address, HEX);
-  Serial.print(", Command = 0x");
-  Serial.println(command, HEX);
+  for(int i = 0; i < numberOfSensors; i++)
+  {
+    uint8_t address = 0x07;  // example address
+    uint8_t command = 0x45;  // example command
 
-  IrSender.sendSamsung(address, command, 0); // send once
+    address = i ;
+    command = sensorData[i] >> 2; // Shift the sensor data to fit into the command
+    
+    Serial.print("Sending Samsung IR: ");
+    Serial.print("Address = 0x");
+    Serial.print(address, HEX);
+    Serial.print(", Command = 0x");
+    Serial.println(command, HEX);
+    IrSender.sendSamsung(address, command, 0); // send once
+    delay(100); // Delay between sends to avoid flooding the receiver
+  }
   delay(2000);
 }
